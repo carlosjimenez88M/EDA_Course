77ad97fac66c55c280f6d6e5108866bd0e3217d2
USER <USER@USER-PC>
2021-11-18 18:41:18 -0500

    tarea jfreile

---
+++ mis_tareas/Jose_Freile_tarea_de_programación_1.py
@@ -0,0 +1,7 @@
+x = float(input("Valor X : "))
+Izq = 5*(-3*x-2)-(-x-3)
+Der = -4*(4*x+5)+13
+
+print ('Valor x = '+ str(x))
+print ('5(-3x-2)-(-x-3)=-4(4x+5)+13')
+print (str(Izq) + '=' + str(Der))
