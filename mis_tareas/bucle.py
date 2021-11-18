fb8cb31db0ab9ed636db950f0e5b52812a66735c
wiroal <wiroal@gmail.com>
2021-11-18 12:21:46 -0500

    Probando scrit del curso

---
+++ mis_tareas/bucle.py
@@ -0,0 +1,16 @@
+# Ejercicio tomado de ED_Course Cuadernos 
+def  ejecutar ():
+    límite  =  1000
+
+    valor_inicial  =  0
+    
+    valor_anexar  =  1  +  valor_inicial
+    
+    '' 'Acá vamos hacer el bucle, esto es interesante' ''
+    while  valor_anexar  <  limite :
+        imprimir ( valor_anexar )
+        valor_inicial  =  valor_inicial  +  1
+        valor_anexar  =  valor_inicial  +  1
+
+if  __name__  ==  '__main__' :
+    correr ()
