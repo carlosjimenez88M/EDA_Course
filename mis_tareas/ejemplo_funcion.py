0930c842e4cfdcf8e5c1916dbdfdd32d54699b02
Daniel Jimenez <cjimenez187@aol.com>
2021-11-18 19:01:37 -0500

    Agrego lo de la clase

---
+++ mis_tareas/ejemplo_funcion.py
@@ -0,0 +1,17 @@
+Peso = float(input("Hey dime tu peso : "))
+Estatura = float(input("Hey dime tu estatura : "))
+
+def BMI(Peso,Estatura) :
+    BMI = Peso/(Estatura**2)
+    if BMI <= 18.5:
+        print('Tu indice de masa corporal es :', BMI, ' Estas flaco')
+
+    elif 18.5 < BMI < 25:
+        print('Tu indice de masa corporal es :', BMI,'Ni fu ni fa')
+
+    elif 25 < BMI < 30:
+        print('Tu indice de masa corporal es :', BMI,' Estamos gorditos!!')
+
+    elif BMI > 30:
+        print('Tu indice de masa corporal es :', BMI,'No sigas asi! vas a explotar ')
+print(BMI(Peso,Estatura))
