89a274f85565d836e24d59ba995bfac1216e110c
JaedFonar <jaime.fonseca@jep.gov.co>
2021-11-20 12:15:52 -0500

    Ajustes_2

--- mis_tareas/Eduardo_Fonseca_Tarea_de_programación_1.py
+++ mis_tareas/Eduardo_Fonseca_Tarea_de_programación_1.py
@@ -1,7 +1,7 @@
 print("Tarea sorpresa")
 # La ecuación a resolver es 5(-3-2)-(-x-3)=-4(-4x+5)+13
 #Parte 1
-5*((-3)-2) + 3= x 
-b = (-4)*((-4*X)+5)+13
+x = 5*((-3)-2) + 3 
+b = (-4)*((-4*x)+5)+13
 print ("la solución es", b)
 # Es un comentario de prueba
