0034b129f255f06906b1001772e4f922e63c6434
German Lozano <glozano_vivas@hotmail.com>
2021-11-24 17:16:06 -0500

    tareas

---
+++ mis_tareas/tarea 32germanlozano.py
@@ -0,0 +1,5 @@
+lst = [] 
+for j in range(1363): 
+    lst.append(j) 
+      
+print(lst) 
---
+++ mis_tareas/tarea 3germanlozano.py
@@ -0,0 +1,9 @@
+def fahrenheit_celcius (f):
+        return(f-32)/1.8
+f = float(input("ingrese los grados farengeit:"))
+c = fahrenheit_celcius (f)
+print(f"los {f} grados farenheit son {c} grados celcius")
+
+
+
+
